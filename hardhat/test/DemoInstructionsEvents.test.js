d5ef4f6790f242e7cd5a99d856072f3d1ce33ba3
lytves <22356770+lytves@users.noreply.github.com>
2022-04-17 00:14:53 +0200

    DemoInstructionsEvents test completes

--- hardhat/test/DemoInstructionsEvents.test.js
+++ hardhat/test/DemoInstructionsEvents.test.js
@@ -9,17 +9,21 @@ describe("DemoInstructionsEvents", function() {
     beforeEach(async function() {
         [owner, other_addr] = await ethers.getSigners();
 
-        const DemoInstructionsEvents = await ethers.getContractFactory("DemoInstructionsEvents", owner);
+        const DemoInstructionsEvents = await ethers.getContractFactory(
+            "DemoInstructionsEvents",
+            owner
+        );
         demo = await DemoInstructionsEvents.deploy();
         await demo.deployed();
-        console.log("deployed");
+        console.log("contract successfully deployed", demo.address);
     });
 
+    // util function to send test transaction
     async function sendMoney(sender) {
         const amount = 100;
         const txData = {
             to: demo.address,
-            value: amount
+            value: amount,
         };
         const tx = await sender.sendTransaction(txData);
         await tx.wait();
@@ -28,6 +32,37 @@ describe("DemoInstructionsEvents", function() {
 
     it("should allow to send money", async function() {
         const [sendMoneyTx, amount] = await sendMoney(other_addr);
-        console.log(sendMoneyTx);
+        console.log("test transaction executed:", sendMoneyTx);
+
+        // demo balance was changed
+        await expect(() => sendMoneyTx).to.changeEtherBalance(demo, amount);
+
+        // events log was emit an event
+        const timestamp = (await ethers.provider.getBlock(sendMoneyTx.blockNumber))
+            .timestamp;
+
+        await expect(sendMoneyTx)
+            .to.emit(demo, "Paid")
+            .withArgs(other_addr.address, amount, timestamp);
+    });
+
+    // the contract's owner can withdraw the money from his contract
+    it("should allow to owner to withdraw funds", async function() {
+        const [_, amount] = await sendMoney(other_addr);
+
+        const tx = await demo.withdrawWithModifierAdvanced(owner.address);
+
+        await expect(() => tx).to.changeEtherBalances(
+            [demo, owner], [-amount, amount]
+        );
+    });
+
+    // the contract's owner can withdraw the money from his contract
+    it("should not allow to other account to withdraw funds", async function() {
+        await sendMoney(other_addr);
+
+        await expect(
+            demo.connect(other_addr).withdrawWithModifierAdvanced(other_addr.address)
+        ).to.be.revertedWith("you are not an owner of the contract!");
     });
 });
